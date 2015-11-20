39d8fc6566615e489884059fd13100814ea2494b
Earl Chew <earl_chew@yahoo.com>
2015-11-20 06:42:53 -0800

    [k9] Enhance test.sh to use valgrind
    Signed-off-by: Earl Chew <earl_chew@yahoo.com>

--- src/test.sh
+++ src/test.sh
@@ -1,207 +1,227 @@
 #!/bin/sh
 
-set -e
+set -eu
 
-TEST()
+testCase()
 {
-    printf '\nTEST - %s\n' "$1"
+    printf '\ntestCase - %s\n' "$1"
 }
 
-TEST_EXIT()
+testExit()
 {
-    if ( shift ; "$@" )
-    then false ; else [ $? -eq $1 ] ; fi
-}
-
-TEST 'Usage'
-TEST_EXIT 1 ./k9 -? -- true
-
-TEST 'Missing -P option value'
-TEST_EXIT 1 ./k9 -P
-
-TEST 'Illegal negative -P option value'
-TEST_EXIT 1 ./k9 -P -2 -- true
-
-TEST 'Valid -P option values'
-./k9 -P -1 -- true
-./k9 -P 0 -- true
-./k9 -P 1 -- true
-
-TEST 'Long --pid option'
-./k9 --pid 1 -- true
-
-TEST 'Missing -p option value'
-TEST_EXIT 1 ./k9 -p
-
-TEST 'Valid -p option value'
-./k9 -d -p pidfile -- true
-
-TEST 'Long --pidfile option'
-./k9 --pidfile pidfile -- true
-
-TEST 'Missing command'
-TEST_EXIT 1 ./k9
-
-TEST 'Simple command'
-REPLY=$(./k9 echo test)
-[ x"$REPLY" = x"test" ]
-
-TEST 'Simple command in test mode'
-REPLY=$(./k9 -T echo test)
-[ x"$REPLY" = x"test" ]
-
-TEST 'Empty pid file'
-rm -f pidfile
-: > pidfile
-./k9 -T -p pidfile -- true
-
-TEST 'Invalid content in pid file'
-rm -f pidfile
-dd if=/dev/zero bs=1K count=1 > pidfile
-./k9 -T -p pidfile -- true
-
-TEST 'Dead process in pid file'
-rm -f pidfile
-sh -c 'echo $$' > pidfile
-./k9 -T -d -p pidfile -- true
-
-TEST 'Aliased process in pid file'
-rm -f pidfile
-( sh -c 'echo $(( $$ + 1))' > pidfile ; sleep 1 ) &
-sleep 1
-./k9 -T -d -p pidfile -- true
-wait
-
-TEST 'Read non-existent pid file'
-rm -f pidfile
-TEST_EXIT 1 ./k9 -T -p pidfile
-
-TEST 'Read malformed pid file'
-rm -f pidfile
-date > pidfile
-TEST_EXIT 1 ./k9 -T -p pidfile
-
-TEST 'Identify processes'
-for REPLY in $(
-  exec sh -c 'echo $$ ; exec ./k9 -T -i -- sh -c '\''echo $$'\' |
-  {
-    read REALPARENT
-    read PARENT ; read CHILD
-    read REALCHILD
-    echo "$REALPARENT/$PARENT $REALCHILD/$CHILD"
-  }) ; do
-
-  [ x"${REPLY%% *}" = x"${REPLY#* }" ]
-done
-
-TEST 'Exit code propagation'
-TEST_EXIT 2 ./k9 -T -- sh -c 'exit 2'
-
-TEST 'Signal propagation'
-TEST_EXIT $((128 + 9)) ./k9 -T -- sh -c 'echo $$ ; kill -9 $$'
-
-TEST 'Untethered child process'
-[ $(./k9 -T -u -- ls -l /proc/self/fd | grep '[0-9]-[0-9]' | wc -l) -eq 4 ]
-
-TEST 'Untethered child process with 8M data'
-[ $(./k9 -T -u -- dd if=/dev/zero bs=8K count=1000 | wc -c) -eq 8192000 ]
-
-TEST 'Tether with new file descriptor'
-[ $(./k9 -T -f - -- ls -l /proc/self/fd | grep '[0-9]-[0-9]' | wc -l) -eq 5 ]
-
-TEST 'Tether using stdout'
-[ $(./k9 -T -- ls -l /proc/self/fd | grep '[0-9]-[0-9]' | wc -l) -eq 4 ]
-
-TEST 'Tether using named stdout'
-[ $(./k9 -T -f 1 -- ls -l /proc/self/fd | grep '[0-9]-[0-9]' | wc -l) -eq 4 ]
-
-TEST 'Tether using stdout with 8M data'
-[ $(./k9 -T -- dd if=/dev/zero bs=8K count=1000 | wc -c) -eq 8192000 ]
-
-TEST 'Tether quietly using stdout with 8M data'
-[ $(./k9 -T -q -- dd if=/dev/zero bs=8K count=1000 | wc -c) -eq 0 ]
-
-TEST 'Tether named in environment'
-[ $(./k9 -T -n TETHER -- printenv | grep TETHER) = "TETHER=1" ]
-
-TEST 'Tether named alone in argument'
-[ $(./k9 -T -n @tether@ -- echo @tether@ | grep '1') = "1" ]
-
-TEST 'Tether named as suffix in argument'
-[ $(./k9 -T -n @tether@ -- echo x@tether@ | grep '1' ) = "x1" ]
-
-TEST 'Tether named as prefix argument'
-[ $(./k9 -T -n @tether@ -- echo @tether@x | grep '1' ) = "1x" ]
-
-TEST 'Tether named as infix argument'
-[ $(./k9 -T -n @tether@ -- echo x@tether@x | grep '1' ) = "x1x" ]
-
-TEST 'Unexpected death of child'
-REPLY=$(
-  exec 3>&1
-  {
-    if ./k9 -T -d -i -- sh -c 'while : ; do : ; done ; exit 0' 3>&- ; then
-      echo 0 >&3
+    if [ x$1 = x0 ] ; then
+        shift ; "$@"
     else
-      echo $? >&3
+        if ( shift ; "$@" ) ; then
+            false
+        else
+            [ $? -eq $1 ]
+        fi
     fi
-  } | {
-    read PID
-    kill -- "$PID"
-  })
-[ x"$REPLY" = x$((128 + 15)) ]
+}
 
-TEST 'Fast signal queueing'
-REPLY=$(
-  SIGNALS="1 2 3 15"
-  sh -c "
-    echo \$\$
-    exec ./k9 -T -d -- sh -c '
-      C=0
-      trap '\\'': \$(( ++C ))'\\'' $SIGNALS
-      echo \$\$
-      while [ \$C -ne $(echo $SIGNALS | wc -w) ] ; do sleep 1 ; done
-      echo \$C'" |
-  {
-    read PARENT
-    read CHILD
-    for SIG in $SIGNALS ; do
-        kill -$SIG -- "$PARENT"
+runWait()
+{
+    $VALGRIND "$@"
+}
+
+runTests()
+{
+    testCase 'Usage'
+    testExit 1 runWait ./k9 -? -- true
+
+    testCase 'Missing -P option value'
+    testExit 1 runWait ./k9 -P
+
+    testCase 'Illegal negative -P option value'
+    testExit 1 runWait ./k9 -P -2 -- true
+
+    testCase 'Valid -P option values'
+    runWait ./k9 -P -1 -- true
+    runWait ./k9 -P 0 -- true
+    runWait ./k9 -P 1 -- true
+
+    testCase 'Long --pid option'
+    runWait ./k9 --pid 1 -- true
+
+    testCase 'Missing -p option value'
+    testExit 1 runWait ./k9 -p
+
+    testCase 'Valid -p option value'
+    runWait ./k9 -d -p pidfile -- true
+
+    testCase 'Long --pidfile option'
+    runWait ./k9 --pidfile pidfile -- true
+
+    testCase 'Missing command'
+    testExit 1 runWait ./k9
+
+    testCase 'Simple command'
+    REPLY=$(runWait ./k9 echo test)
+    [ x"$REPLY" = x"test" ]
+
+    testCase 'Simple command in test mode'
+    REPLY=$(runWait ./k9 -T echo test)
+    [ x"$REPLY" = x"test" ]
+
+    testCase 'Empty pid file'
+    rm -f pidfile
+    : > pidfile
+    runWait ./k9 -T -p pidfile -- true
+
+    testCase 'Invalid content in pid file'
+    rm -f pidfile
+    dd if=/dev/zero bs=1K count=1 > pidfile
+    runWait ./k9 -T -p pidfile -- true
+
+    testCase 'Dead process in pid file'
+    rm -f pidfile
+    sh -c 'echo $$' > pidfile
+    runWait ./k9 -T -d -p pidfile -- true
+
+    testCase 'Aliased process in pid file'
+    rm -f pidfile
+    ( sh -c 'echo $(( $$ + 1))' > pidfile ; sleep 1 ) &
+    sleep 1
+    runWait ./k9 -T -d -p pidfile -- true
+    wait
+
+    testCase 'Read non-existent pid file'
+    rm -f pidfile
+    testExit 1 runWait ./k9 -T -p pidfile
+
+    testCase 'Read malformed pid file'
+    rm -f pidfile
+    date > pidfile
+    testExit 1 runWait ./k9 -T -p pidfile
+
+    testCase 'Identify processes'
+    for REPLY in $(
+      exec sh -c 'echo $$ ; exec ./k9 -T -i -- sh -c '\''echo $$'\' |
+      {
+        read REALPARENT
+        read PARENT ; read CHILD
+        read REALCHILD
+        echo "$REALPARENT/$PARENT $REALCHILD/$CHILD"
+      }) ; do
+
+      [ x"${REPLY%% *}" = x"${REPLY#* }" ]
     done
-    read COUNT
-    echo "$COUNT"
-  })
-[ x"$REPLY" = x"4" ]
 
-TEST 'Slow signal queueing'
-REPLY=$(
-  SIGNALS="1 2 3 15"
-  sh -c "
-    echo \$\$
-    exec ./k9 -T -d -- sh -c '
-      C=0
-      trap '\\'': \$(( ++C ))'\\'' $SIGNALS
-      echo \$\$
-      while [ \$C -ne $(echo $SIGNALS | wc -w) ] ; do sleep 1 ; done
-      echo \$C'" |
-  {
-    read PARENT
-    read CHILD
-    for SIG in $SIGNALS ; do
-        kill -$SIG -- "$PARENT"
-        sleep 1
-    done
-    read COUNT
-    echo "$COUNT"
-  })
-[ x"$REPLY" = x"4" ]
+    testCase 'Exit code propagation'
+    testExit 2 runWait ./k9 -T -- sh -c 'exit 2'
 
-TEST 'Timeout with data that must be flushed after 6s'
-REPLY=$(
-    /usr/bin/time -p ./k9 -T -t 4 -- sh -c 'trap "" 15 ; sleep 6' 2>&1 |
-    grep real)
-REPLY=${REPLY%%.*}
-REPLY=${REPLY##* }
-[ "$REPLY" -ge 6 ]
+    testCase 'Signal propagation'
+    testExit $((128 + 9)) runWait ./k9 -T -- sh -c 'echo $$ ; kill -9 $$'
 
-./k9 -T -p pidfile -- true
+    testCase 'Untethered child process'
+    [ $(runWait ./k9 -T -u -- ls -l /proc/self/fd | grep '[0-9]-[0-9]' | wc -l) -eq 4 ]
+
+    testCase 'Untethered child process with 8M data'
+    [ $(runWait ./k9 -T -u -- dd if=/dev/zero bs=8K count=1000 | wc -c) -eq 8192000 ]
+
+    testCase 'Tether with new file descriptor'
+    [ $(runWait ./k9 -T -f - -- ls -l /proc/self/fd | grep '[0-9]-[0-9]' | wc -l) -eq 5 ]
+
+    testCase 'Tether using stdout'
+    [ $(runWait ./k9 -T -- ls -l /proc/self/fd | grep '[0-9]-[0-9]' | wc -l) -eq 4 ]
+
+    testCase 'Tether using named stdout'
+    [ $(runWait ./k9 -T -f 1 -- ls -l /proc/self/fd | grep '[0-9]-[0-9]' | wc -l) -eq 4 ]
+
+    testCase 'Tether using stdout with 8M data'
+    [ $(runWait ./k9 -T -- dd if=/dev/zero bs=8K count=1000 | wc -c) -eq 8192000 ]
+
+    testCase 'Tether quietly using stdout with 8M data'
+    [ $(runWait ./k9 -T -q -- dd if=/dev/zero bs=8K count=1000 | wc -c) -eq 0 ]
+
+    testCase 'Tether named in environment'
+    [ $(runWait ./k9 -T -n TETHER -- printenv | grep TETHER) = "TETHER=1" ]
+
+    testCase 'Tether named alone in argument'
+    [ $(runWait ./k9 -T -n @tether@ -- echo @tether@ | grep '1') = "1" ]
+
+    testCase 'Tether named as suffix in argument'
+    [ $(runWait ./k9 -T -n @tether@ -- echo x@tether@ | grep '1' ) = "x1" ]
+
+    testCase 'Tether named as prefix argument'
+    [ $(runWait ./k9 -T -n @tether@ -- echo @tether@x | grep '1' ) = "1x" ]
+
+    testCase 'Tether named as infix argument'
+    [ $(runWait ./k9 -T -n @tether@ -- echo x@tether@x | grep '1' ) = "x1x" ]
+
+    testCase 'Unexpected death of child'
+    REPLY=$(
+      exec 3>&1
+      {
+        if $VALGRIND./k9 -T -d -i -- sh -c '
+            while : ; do : ; done ; exit 0' 3>&- ; then
+          echo 0 >&3
+        else
+          echo $? >&3
+        fi
+      } | {
+        read PID
+        kill -- "$PID"
+      })
+    [ x"$REPLY" = x$((128 + 15)) ]
+
+    testCase 'Fast signal queueing'
+    REPLY=$(
+      SIGNALS="1 2 3 15"
+      sh -c "
+        echo \$\$
+        exec $VALGRIND ./k9 -T -d -- sh -c '
+          C=0
+          trap '\\'': \$(( ++C ))'\\'' $SIGNALS
+          echo \$\$
+          while [ \$C -ne $(echo $SIGNALS | wc -w) ] ; do sleep 1 ; done
+          echo \$C'" |
+      {
+        read PARENT
+        read CHILD
+        for SIG in $SIGNALS ; do
+            kill -$SIG -- "$PARENT"
+        done
+        read COUNT
+        echo "$COUNT"
+      })
+    [ x"$REPLY" = x"4" ]
+
+    testCase 'Slow signal queueing'
+    REPLY=$(
+      SIGNALS="1 2 3 15"
+      sh -c "
+        echo \$\$
+        exec $VALGRIND ./k9 -T -d -- sh -c '
+          C=0
+          trap '\\'': \$(( ++C ))'\\'' $SIGNALS
+          echo \$\$
+          while [ \$C -ne $(echo $SIGNALS | wc -w) ] ; do sleep 1 ; done
+          echo \$C'" |
+      {
+        read PARENT
+        read CHILD
+        for SIG in $SIGNALS ; do
+            kill -$SIG -- "$PARENT"
+            sleep 1
+        done
+        read COUNT
+        echo "$COUNT"
+      })
+    [ x"$REPLY" = x"4" ]
+
+    testCase 'Timeout with data that must be flushed after 6s'
+    REPLY=$(
+        /usr/bin/time -p $VALGRIND ./k9 -T -t 4 -- sh -c '
+            trap "" 15 ; sleep 6' 2>&1 | grep real)
+    REPLY=${REPLY%%.*}
+    REPLY=${REPLY##* }
+    [ "$REPLY" -ge 6 ]
+}
+
+VALGRIND=
+runTests
+
+VALGRIND="valgrind --error-exitcode=128 --leak-check=yes"
+runTests
