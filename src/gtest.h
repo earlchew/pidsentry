2b1e410915d9bdc356901444106d0172aa816fb3
Earl Chew <earl_chew@yahoo.com>
2016-01-17 15:07:14 -0800

    [k9] Ensure that gtest/gtest.h content is used
    Signed-off-by: Earl Chew <earl_chew@yahoo.com>

--- src/gtest.h
+++ src/gtest.h
@@ -4,6 +4,14 @@
 #ifndef HEADERCHECK
 #define GTEST_HAS_PTHREAD 0
 #include "gtest/gtest.h"
+
+// Ensure that there is code that depends on the successful inclusion of
+// the underlying gtest header so that the delete-orphaned-includes.sh
+// script will not remove the inclusion.
+
+namespace {
+typedef ::testing::Test K9Test_T_;
+}
 #endif
 
 #endif /* K9_GTEST_H */
