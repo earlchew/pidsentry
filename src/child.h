5a47676b4cfd4758ed0bbc44e7d84bd7331d9708
Earl Chew <earl_chew@yahoo.com>
2016-02-13 10:30:57 -0800

    [k9] Move code for child into child.c
    Signed-off-by: Earl Chew <earl_chew@yahoo.com>

---
+++ src/child.h
@@ -0,0 +1,91 @@
+/* -*- c-basic-offset:4; indent-tabs-mode:nil -*- vi: set sw=4 et: */
+/*
+// Copyright (c) 2016, Earl Chew
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the names of the authors of source code nor the names
+//       of the contributors to the source code may be used to endorse or
+//       promote products derived from this software without specific
+//       prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL EARL CHEW BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#ifndef CHILD_H
+#define CHILD_H
+
+#include "pipe_.h"
+
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct StdFdFiller;
+struct SocketPair;
+
+/* -------------------------------------------------------------------------- */
+struct ChildProcess
+{
+    pid_t mPid;
+    pid_t mPgid;
+
+    struct Pipe  mChildPipe_;
+    struct Pipe* mChildPipe;
+    struct Pipe  mTetherPipe_;
+    struct Pipe* mTetherPipe;
+};
+
+/* -------------------------------------------------------------------------- */
+void
+createChild(struct ChildProcess *self);
+
+void
+reapChild(struct ChildProcess *self);
+
+int
+forkChild(
+    struct ChildProcess  *self,
+    char                **aCmd,
+    struct StdFdFiller   *aStdFdFiller,
+    struct Pipe          *aSyncPipe,
+    struct SocketPair    *aUmbilicalSocket);
+
+void
+killChild(struct ChildProcess *self, int aSigNum);
+
+void
+closeChildTether(struct ChildProcess *self);
+
+void
+monitorChildUmbilical(struct ChildProcess *self, pid_t aParentPid);
+
+void
+monitorChild(struct ChildProcess *self, struct File *aUmbilicalFile);
+
+int
+closeChild(struct ChildProcess *self);
+
+/* -------------------------------------------------------------------------- */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CHILD_H */
