90ab9a4947dddad9f63eb46c651d9e97bce40b6f
Earl Chew <earl_chew@yahoo.com>
2016-04-16 12:17:41 -0700

    [parent] Introduce struct ParentProcess
    Signed-off-by: Earl Chew <earl_chew@yahoo.com>

---
+++ src/parent.c
@@ -0,0 +1,115 @@
+/* -*- c-basic-offset:4; indent-tabs-mode:nil -*- vi: set sw=4 et: */
+/*
+// Copyright (c) 2016, Earl Chew
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the names of the authors of source code nor the names
+//       of the contributors to the source code may be used to endorse or
+//       promote products derived from this software without specific
+//       prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL EARL CHEW BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "parent.h"
+
+#include "error_.h"
+#include "timekeeping_.h"
+#include "process_.h"
+#include "thread_.h"
+
+#include <stdlib.h>
+
+/* -------------------------------------------------------------------------- */
+static void *
+monitorParent_(void *self_)
+{
+    struct ParentProcess *self = self_;
+
+    debug(0,
+          "watching parent pid %" PRId_Pid,
+          FMTd_Pid(self->mParentPid));
+
+    while (1)
+    {
+        monotonicSleep(Duration(NSECS(Seconds(3))));
+
+        struct Pid parentPid = ownProcessParentId();
+
+        if (1 == parentPid.mPid)
+        {
+            /* If the parent has terminated, terminate the agent process
+             * to trigger either the sentry, if it is running in a
+             * separate process, or the umbilical to clean up the
+             * child process. */
+
+            if (self->mParentPid.mPid)
+                warn(0,
+                     "Parent pid %" PRId_Pid " terminated",
+                     FMTd_Pid(self->mParentPid));
+            else
+                warn(0, "Parent terminated");
+
+            exitProcess(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
+
+/* -------------------------------------------------------------------------- */
+int
+createParent(struct ParentProcess *self)
+{
+    int rc = - 1;
+
+    /* This process might already have been orphaned since it was created,
+     * so its original parent might be lost. As a consequence, only treat
+     * init(8) as an adoptive parent. */
+
+    self->mParentPid = ownProcessParentId();
+    if (1 == self->mParentPid.mPid)
+        self->mParentPid = Pid(0);
+
+    createThread(&self->mThread_, 0, &monitorParent_, self);
+    self->mThread = &self->mThread_;
+
+    rc = 0;
+
+Finally:
+
+    FINALLY({});
+
+    return rc;
+}
+
+/* -------------------------------------------------------------------------- */
+void
+closeParent(struct ParentProcess *self)
+{
+    if (self)
+    {
+        cancelThread(self->mThread);
+
+        ABORT_UNLESS(
+            PTHREAD_CANCELED == joinThread(self->mThread));
+    }
+}
+
+/* -------------------------------------------------------------------------- */
